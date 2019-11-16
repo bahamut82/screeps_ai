d630947eecee9f4b6fee23359d363ab5dfe78e65
Bahamut82 <6079229+bahamut82@users.noreply.github.com>
2019-11-16 03:49:59 +0100

    added very ugly logic for tagging a target to a creep and only allow 1 creep to tag this target

--- screeps_ai_js/task.pickup.js
+++ screeps_ai_js/task.pickup.js
@@ -1,6 +1,16 @@
 var taskManager = require('task.manager');
 var task = 'pickup';
 
+var cleanup = function(creep) {
+	if ( creep.Memory.pickupTarget ) {
+		if ( !Game.getObjectById(creep.Memory.pickupTarget) )
+			delete creep.room.Memory.taggedTargets[creep.Memory.pickupTarget];
+		else
+			creep.room.Memory.taggedTargets[creep.Memory.pickupTarget]--;
+	}
+	delete creep.Memory.pickupTarget;
+};
+
 var pickupResource = {
 
     /** @param {Creep} creep **/
@@ -11,24 +21,52 @@ var pickupResource = {
         }
 	    if ( creep.store.getFreeCapacity() > 0 ) {
 	        taskManager.startTask(creep, task);
-	        // TODO: add other resource types (initial implementation made only for RESOURCE_ENERGY)
-	        var isTombstone = true;
 	        var target = undefined;
-	        // find non-empty tombstone (at least 10 energy)
-	        target = creep.pos.findClosestByPath(FIND_TOMBSTONES, { filter : function(tomb) { return tomb.store[RESOURCE_ENERGY] > 10 } });
 
+	        var previousTargetId = creep.Memory.pickupTarget;
+	        if ( previousTargetId ) {
+	        	target = Game.getObjectById( previousTargetId );
+	        }
+
+	        // TODO: add other resource types (initial implementation made only for RESOURCE_ENERGY)
 	        if ( !target ) {
-	        	isTombstone = false;
-	        	target = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES, { filter: function(resource) { return resource.resourceType === RESOURCE_ENERGY } } );
+	        	if ( previousTargetId ) {
+	        		// remove previous target from room-memory's taggedTargets
+	        		if ( creep.room.Memory.taggedTargets[previousTargetId] )
+	        			delete creep.room.Memory.taggedTargets[previousTargetId];
+	        	}
+		        // find non-empty, untagged tombstone
+		        target = creep.pos.findClosestByPath(FIND_TOMBSTONES, { filter : function(tomb) { 
+			        	return creep.room.Memory.taggedTargets && creep.room.Memory.taggedTargets[tomb.id] < 1
+			        		&& tomb.store[RESOURCE_ENERGY] > 0
+			        }
+			    });
 	        }
 
 	        if ( !target ) {
+	        	// find untagged resource (for now only RESOURCE_ENERGY)
+	        	target = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES, { filter: function(resource) {
+		        		return creep.room.Memory.taggedTargets && creep.room.Memory.taggedTargets[resource.id] < 1
+		        			&& resource.resourceType === RESOURCE_ENERGY
+		        	}
+	        	});
+	        }
+
+	        // abort task when no more target can be found
+	        if ( !target ) {
+	        	cleanup(creep);
 	        	taskManager.finishTask(creep);
 	        	return;
 	        }
 
+	        creep.Memory.pickupTarget = target.id;
+	        if ( creep.room.Memory.taggedTargets ) {
+	        	creep.room.Memory.taggedTargets[target.id] = 1;
+	        }
+
+
 	        var pickupResult = undefined;
-	        if ( isTombstone ) {
+	        if ( target instanceof Tombstone ) {
 	        	pickupResult = creep.withdraw(target, RESOURCE_ENERGY);
 	        } else {
 	        	pickupResult = creep.pickup(target);
@@ -36,6 +74,7 @@ var pickupResource = {
         	if ( pickupResult === ERR_NOT_IN_RANGE ) {
         		creep.moveTo(target);
         	} else if ( pickupResult === ERR_FULL ) {
+        		cleanup(creep);
         		taskManager.finishTask(creep);
         	} else if ( pickupResult !== OK ) {
         		console.log("task.pickup: unhandled withdrawResult = " + withdrawResult);
@@ -44,6 +83,7 @@ var pickupResource = {
             return;
         }
 
+   		cleanup(creep);
         taskManager.finishTask(creep);
 
 	}
